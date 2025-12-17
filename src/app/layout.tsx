ccec1479535c4e3aefc96b2440bbdde4d3b8848f
HarshKumarYadav0563 <harsh0563.be21@chitkara.edu.in>
2025-12-17 20:33:56 +0530

    Feat: Add Google Global Site Tag (gtag.js)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -94,6 +95,19 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0EKXZN63DQ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-0EKXZN63DQ');
+          `}
+        </Script>
       </body>
     </html>
   );
